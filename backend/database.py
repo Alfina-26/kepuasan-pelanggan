62c4bb322577370055ab18f8244331b026542205
Alfina-26 <berlianalfina91@gmail.com>
2026-05-14 22:41:25 +0700

    update

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:pinaberli2629@localhost:5432/kepuasan_pelanggan"
+load_dotenv()  # baca dari file .env saat lokal
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
